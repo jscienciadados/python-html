93a95d769465f5455279cb82b9c8ef778f48f7f3
jscienciadados <joaosouzaps9@gmail.com>
2021-06-19 17:44:25 +0000

    solução do blog

--- flaskr.py
+++ flaskr.py
@@ -1,20 +1,21 @@
 import sqlite3
 from flask import Flask, g, render_template, request
 
-# Configuration
+# Configurações
 
 DATABASE = './flaskr.db'
 SECRET_KEY = "pudim"
 USERNAME = 'admin'
 PASSWORD = 'admin'
 
-# Application
+# Aplicação
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(DATABASE)
+
 
 @app.before_request
 def before():
@@ -24,17 +25,17 @@ def before():
 def after(exception):
     g.db.close()
 
-
 @app.route('/')
 def index():
-    sql = 'SELECT titulo, texto from entradas ordeby id desc'
+    sql = 'SELECT titulo, texto from entradas order by id desc'
     cur = g.db.execute(sql)
     entradas = [dict(titulo=titulo, texto=texto) for titulo, texto in cur.fetchall()]
-    return render_template('index.html')  
+    return render_template('index.html', entradas=entradas)
 
-@app.route('inserir', methods=['POST'])
-def inserir_post():
-    sql = 'INSERT INTO entradas(titulo, texto) values (? ?)'
-    g.db.execute(sql, [request.form['titulo'], request.form['texto']])  
+@app.route('/inserir', methods=['POST'])
+def inserir():
+    sql = "INSERT INTO entradas(titulo, texto) values (?, ?)"
+    g.db.execute(sql, [request.form['titulo'], request.form['texto']])
     g.db.commit()
-    return render_template('index.html')   
+    return render_template('index.html')
+    
